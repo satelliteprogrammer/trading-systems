4bf795664687358e36f1c739bae7555c28c158f1
Luís Murta <luis@murta.dev>
2026-07-11 11:24:38 +0100

    refactor(quantcup): extract execute lambda into a free function

--- tests/benchmark/quantcup_engine.cpp
+++ tests/benchmark/quantcup_engine.cpp
@@ -16,6 +16,21 @@ namespace {
 Book book;
 t_orderid next_id;
 
+auto execute(t_order const &order, Order const &order_, Price price, std::uint64_t quantity) {
+    t_execution exec;
+    std::strncpy(exec.symbol, order.symbol, STRINGLEN);
+    exec.price = price;
+    exec.size = quantity;
+
+    exec.side = order.side;
+    std::strncpy(exec.trader, order.trader, STRINGLEN);
+    execution(exec);
+
+    exec.side = (order.side == 1) ? 0 : 1;
+    std::strncpy(exec.trader, order_.trader_id.data(), STRINGLEN);
+    execution(exec);
+}
+
 } // namespace
 
 void init() {
@@ -31,30 +46,13 @@ void destroy() {
 
 auto limit(t_order order) -> t_orderid {
     t_orderid id = next_id++;
-    t_size remaining = order.size;
-
-    auto execute = [&order](Order const &order_, Price price, std::uint64_t quantity) -> void {
-        t_execution exec;
-        // NOLINTNEXTLINE(bugprone-suspicious-stringview-data-usage)
-        std::strncpy(exec.symbol, order.symbol, STRINGLEN);
-        exec.price = price;
-        exec.size = quantity;
-
-        exec.side = order.side;
-        std::strncpy(exec.trader, order.trader, STRINGLEN);
-        execution(exec);
-
-        exec.side = (order.side == 1) ? 0 : 1;
-        std::strncpy(exec.trader, order_.trader_id.data(), STRINGLEN);
-        execution(exec);
-    };
 
     if (is_ask(order.side) != 0) {
         SellOrder sell{id, order.size, {}, std::to_array(order.trader), order.price};
-        [[maybe_unused]] auto res = book.add(sell, execute);
+        [[maybe_unused]] auto res = book.add(sell, std::bind_front(&execute, order));
     } else {
         BuyOrder buy{id, order.size, {}, std::to_array(order.trader), order.price};
-        [[maybe_unused]] auto res = book.add(buy, execute);
+        [[maybe_unused]] auto res = book.add(buy, std::bind_front(&execute, order));
     }
 
     return id;
